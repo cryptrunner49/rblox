134a336623f70b8b6a71c87ceca05d7296b4083d
cryptrunner49 <cryptrunner49@proton.me>
2025-02-23 11:31:58 +0000

    Add TokenType

--- rblox.rb
+++ rblox.rb
@@ -1,39 +1,72 @@
+# frozen_string_literal: true
+
 require_relative 'utils/string_colors'
 
 class Lox
-  def main(args)
-    if args.length > 1
-      puts "Usage: rblox [scipt]"
-      exit(64)
-    elsif args.length == 1
-      run_file(args[0])
-    else
-      run_prompt
+  class << self
+    attr_accessor :had_error
+
+    @had_error = false
+
+    def main(args)
+      if args.length > 1
+        puts 'Usage: rblox [script]'
+        exit(64)
+      elsif args.length == 1
+        run_file(args[0])
+      else
+        run_prompt
+      end
     end
-  end
 
-  def run_file(file_path)
-    content = File.read(file_path, encoding: 'UTF-8')
-    run(content)
-  end
+    def run_file(file_path)
+      content = File.read(file_path, encoding: 'UTF-8')
+      run(content)
 
-  def run_prompt
-    puts "rblox 0.0.1"
-    puts "CTRL+C to quit"
-    loop do
-      print "rblox>> ".red
-      line = gets&.chomp
-      break if line.nil? || line.empty?
-      run(line)
+      # Indicate an error in the exit code
+      exit(65) if Lox.had_error
     end
-  end
 
-  def run(_source)
-    # Placeholder for actual script execution logic
+    def run_prompt
+      puts 'rblox 0.0.1'
+      puts 'CTRL+C to quit'
+      loop do
+        print 'rblox>> '.red
+        line = gets&.chomp
+        break if line.nil? || line.empty?
+
+        run(line)
+        false
+      end
+    end
+
+    def run(_source)
+      # Placeholder for actual script execution logic
+      # Scanner scanner = new Scanner(source);
+      # List<token> tokens = scanner.scanTokens();
+
+      # For now, just print the tokens
+      # for (Token token : tokens) {
+      #  System.out.println(token);
+      # }
+    end
+
+    def error(line, message)
+      report(line, '', message)
+    end
+
+    private
+
+    def report(line, where, message)
+      warn "[line #{line}] Error#{where}: #{message}"
+      @had_error = true
+    end
   end
 end
 
 # Call main method with command-line arguments
-#Lox.main(ARGV)
-lox = Lox.new
-lox.main(ARGV)
+Lox.main(ARGV)
+
+# Example Usage:
+Lox.error(10, 'Unexpected character')
+puts Lox.had_error # => true
--- utils/string_colors.rb
+++ utils/string_colors.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 module StringColors
   COLORS = {
-    black:   "\e[30m",
-    red:     "\e[31m",
-    green:   "\e[32m",
-    yellow:  "\e[33m",
-    blue:    "\e[34m",
+    black: "\e[30m",
+    red: "\e[31m",
+    green: "\e[32m",
+    yellow: "\e[33m",
+    blue: "\e[34m",
     magenta: "\e[35m",
-    cyan:    "\e[36m",
-    white:   "\e[37m",
-    reset:   "\e[0m"
-  }
+    cyan: "\e[36m",
+    white: "\e[37m",
+    reset: "\e[0m"
+  }.freeze
 
   COLORS.each do |color, code|
     define_method(color) do
